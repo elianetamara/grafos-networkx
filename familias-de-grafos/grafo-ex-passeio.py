c9f2b88538706df1bceaf346c0b2c9e1062eb883
eliane <eliane.oliveira.ext@synchro.com.br>
2023-08-05 16:50:46 -0300

    refactoring

--- familias-de-grafos/grafo-ex-passeio.py
+++ familias-de-grafos/grafo-ex-passeio.py
@@ -3,27 +3,16 @@ import sys
 sys.path.append("/home/eliane/ufcg/grafos-networkx/utils")
 from utils import draw_graph
 
-G4 = nx.Graph()
+grafo = nx.Graph()
 
 
-G4.add_node('n0')
-G4.add_node('n1')
-G4.add_node('n2')
-G4.add_node('n3')
-G4.add_node('n4')
-G4.add_node('n5')
+grafo.add_nodes_from(['n0','n1','n2','n3', 'n4', 'n5'])
 
-G4.add_edge('n0','n5')
-G4.add_edge('n0','n3')
-G4.add_edge('n0','n4')
-G4.add_edge('n1','n2')
-G4.add_edge('n1','n5')
-G4.add_edge('n1','n4')
-G4.add_edge('n2','n5')
-G4.add_edge('n2','n3')
-G4.add_edge('n3','n4')
+grafo.add_edges_from([('n0','n5'),('n0','n3'),('n0','n4'),('n1','n2'), ('n1','n5'),
+                   ('n1','n4'), ('n2','n5'), ('n2','n3'), ('n3','n4')])
 
-draw_graph(G4)
+
+draw_graph(grafo)
 
 lists = [ # sequências de vértices a serem testadas
     ['n0', 'n3', 'n0', 'n5'],
@@ -31,11 +20,11 @@ lists = [ # sequências de vértices a serem testadas
     ['n1', 'n4', 'n3', 'n2', 'n1'],
     ['n2', 'n0', 'n4']]
 for l in lists:
-  if nx.is_path(G4,l):
+  if nx.is_path(grafo,l):
     print(f"{l} é um passeio", end="; ")
   else:
     print(f"{l} não é um passeio", end="; ")
-  if nx.is_simple_path(G4,l):
+  if nx.is_simple_path(grafo,l):
     print(f"é um caminho\n")
   else:
     print(f"não é um caminho\n")
--- introducao-grafos/grafo-atributos.py
+++ introducao-grafos/grafo-atributos.py
@@ -3,22 +3,22 @@ import sys
 sys.path.append("/home/eliane/ufcg/grafos-networkx/utils")
 from utils import draw_graph
 
-G4 = nx.Graph()
+grafo = nx.Graph()
 
 
-G4.add_node('n0', label ='a', fillcolor = 'grey')
-G4.add_node('n1', label ='b', fillcolor = 'blue')
-G4.add_node('n2', label ='c', fillcolor = 'darkgreen')
-G4.add_node('n3', label ='d', fillcolor = 'red')
+grafo.add_node('n0', label ='a', fillcolor = 'grey')
+grafo.add_node('n1', label ='b', fillcolor = 'blue')
+grafo.add_node('n2', label ='c', fillcolor = 'darkgreen')
+grafo.add_node('n3', label ='d', fillcolor = 'red')
 
-G4.add_edge('n0','n1',label = 'ab')
-G4.add_edge('n1','n2',label = 'bc')
-G4.add_edge('n0','n3',label = 'ad')
-G4.add_edge('n1','n3',label = 'bd')
-G4.add_edge('n2','n3',label = 'cd')
+grafo.add_edge('n0','n1',label = 'ab')
+grafo.add_edge('n1','n2',label = 'bc')
+grafo.add_edge('n0','n3',label = 'ad')
+grafo.add_edge('n1','n3',label = 'bd')
+grafo.add_edge('n2','n3',label = 'cd')
 
-print(G4.nodes(data=True))
-print(G4.edges(data=True))
-print(nx.get_node_attributes(G4,'label'))
+print(grafo.nodes(data=True))
+print(grafo.edges(data=True))
+print(nx.get_node_attributes(grafo,'label'))
 
-draw_graph(G4, node_labels=nx.get_node_attributes(G4,'label'), edge_labels=nx.get_edge_attributes(G4,'label'))
+draw_graph(grafo, node_labels=nx.get_node_attributes(grafo,'label'), edge_labels=nx.get_edge_attributes(grafo,'label'))
